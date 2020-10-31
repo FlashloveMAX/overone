c156f468b0287e7868719cf36045787d295e8425
FlashloveMAX <maximflashlove@gmail.com>
2020-10-31 10:46:51 +0300

    'fix'

--- school/group.py
+++ school/group.py
@@ -55,19 +55,19 @@ class Group:
     def count_teacher(self):
         return len(self.__teachers)
 
-s1 = Student("Ivan", "Petrov", 19, 4)
-s2 = Student("Petr", "Petrov", 19, 4)
-t1 = Teacher("Alex", "Sidorov", 76, 34)
-t2 = Teacher("Semen", "Sidorov", 34, 4)
-
-g1 = Group(1, [s1, s2], [t1, t2])
-print(g1)
-g1.add_student(Student("Max", "Ivanov", 19, 8.1))
-g1.remove_student("Petr", "Petrov")
-for i in g1.students:
-    print(i)
-
-print(g1.add_teacher(Teacher("Max", "Ivanov", 39, 19)))
-for i in g1.teachers:
-    print(i)
-print(g1.count_teacher())
+# s1 = Student("Ivan", "Petrov", 19, 4)
+# s2 = Student("Petr", "Petrov", 19, 4)
+# t1 = Teacher("Alex", "Sidorov", 76, 34)
+# t2 = Teacher("Semen", "Sidorov", 34, 4)
+#
+# g1 = Group(1, [s1, s2], [t1, t2])
+# print(g1)
+# g1.add_student(Student("Max", "Ivanov", 19, 8.1))
+# g1.remove_student("Petr", "Petrov")
+# for i in g1.students:
+#     print(i)
+#
+# print(g1.add_teacher(Teacher("Max", "Ivanov", 39, 19)))
+# for i in g1.teachers:
+#     print(i)
+# print(g1.count_teacher())
